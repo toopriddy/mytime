9f87888f7ed62cf1b5adc64e2c1cfc81e93fd843
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-01-12 01:10:57 +0000

    added geocacheing in a seperate class

---
+++ Classes/GeocacheViewController.h
@@ -0,0 +1,25 @@
+//
+//  MapViewController.h
+//  MyTime
+//
+//  Created by Brent Priddy on 8/30/08.
+//  Copyright 2008 PG Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MapView.h"
+
+@interface GeocacheViewController : UIView <MapWebViewDelegate>
+{
+	MapView *mapView;
+	NSMutableDictionary *call;
+	UIActivityIndicatorView *progressView;
+}
+
+@property (nonatomic, retain) MapView *mapView;
+@property (nonatomic, retain) NSMutableDictionary *call;
+@property (nonatomic, retain) UIActivityIndicatorView *progressView;
+
+- (id)initWithCall:(NSMutableDictionary *)call;
+
+@end
