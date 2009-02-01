aaa3aae8bade73f22532931ec497c1a2a8e723f6
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-02-01 22:00:19 +0000

    adding start of sorting by metadata

---
+++ Classes/CallsSortedByFilterDataSource.h
@@ -0,0 +1,20 @@
+//
+//  CallsSortedByStudyViewDataSource.h
+//  MyTime
+//
+//  Created by Brent Priddy on 9/24/08.
+//  Copyright 2008 Priddy Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseSortedCallsDataSource.h"
+
+@interface CallsSortedByFilterDataSource : BaseSortedCallsDataSource  
+{
+}
+
+- (id)init;
+- (void)dealloc;
+
+@end
+
