9a81a5b9ad727f4d71cf29cf4a3d326c5094f5a6
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-02-02 06:41:54 +0000

    Fixed manual mapping and made map detail look like google maps

--- trunk/Classes/MapViewController.m
+++ trunk/Classes/MapViewController.m
@@ -24,6 +24,7 @@
 	self = [super init];
 	if (self)
 	{
+		_shouldReloadMarkers = NO;
 		// this title will appear in the navigation bar
 		self.title = theTitle;
 		self.call = theCall;
@@ -38,6 +39,7 @@
 	self = [super init];
 	if (self)
 	{
+		_shouldReloadMarkers = NO;
 		// this title will appear in the navigation bar
 		self.title = theTitle;
 		self.call = nil;
@@ -53,8 +55,7 @@
 	[[Geocache sharedInstance] removeDelegate:self];
 	self.mapView = nil;
 	self.call = nil;
-	
-	
+		
 	[super dealloc];
 }
 
@@ -86,22 +87,16 @@
 	
 	if(call)
 	{
-		NSString *str = [self getAddressFromCall:call useHtml:NO];
-		NSString *info = [self getInfoFromCall:call];
-		if(str)
+		NSString *latLong = [call objectForKey:CallLattitudeLongitude];
+		if(latLong)
 		{
-			NSString *latLong = [call objectForKey:CallLattitudeLongitude];
-			if(latLong)
-			{
-				RMMarker *marker = [[[RMMarker alloc] initWithKey:RMMarkerBlueKey] autorelease];
-				[marker setTextLabel:info];
-				[marker hideLabel];
-				CLLocationCoordinate2D point;
-				NSArray *stringArray = [latLong componentsSeparatedByString:@", "];
-				point.latitude = [[stringArray objectAtIndex:0] doubleValue];
-				point.longitude = [[stringArray objectAtIndex:1] doubleValue];
-				[markerManager addMarker:marker AtLatLong:point];
-			}
+			RMMarker *marker = [[[RMMarker alloc] initWithKey:RMMarkerBlueKey] autorelease];
+			[marker setData:call];
+			CLLocationCoordinate2D point;
+			NSArray *stringArray = [latLong componentsSeparatedByString:@", "];
+			point.latitude = [[stringArray objectAtIndex:0] doubleValue];
+			point.longitude = [[stringArray objectAtIndex:1] doubleValue];
+			[markerManager addMarker:marker AtLatLong:point];
 		}
 	}
 	else
@@ -111,37 +106,33 @@
 		
 		while ( (theCall = [e nextObject]) ) 
 		{
-			NSString *str = [self getAddressFromCall:theCall useHtml:NO];
-			if(str)
+			NSString *latLong = [theCall objectForKey:CallLattitudeLongitude];
+			if(latLong && ![latLong isEqualToString:@"nil"])
 			{
-				NSString *latLong = [theCall objectForKey:CallLattitudeLongitude];
-				if(latLong && ![latLong isEqualToString:@"nil"])
+				RMMarker *marker = [[[RMMarker alloc] initWithKey:RMMarkerBlueKey] autorelease];
+				[marker setData:theCall];
+				CLLocationCoordinate2D point;
+				NSArray *stringArray = [latLong componentsSeparatedByString:@", "];
+				point.latitude = [[stringArray objectAtIndex:0] doubleValue];
+				point.longitude = [[stringArray objectAtIndex:1] doubleValue];
+				[markerManager addMarker:marker AtLatLong:point];
+				[mapView moveToLatLong:point];
+				if(init)
 				{
-					RMMarker *marker = [[[RMMarker alloc] initWithKey:RMMarkerBlueKey] autorelease];
-					[marker setData:theCall];
-					CLLocationCoordinate2D point;
-					NSArray *stringArray = [latLong componentsSeparatedByString:@", "];
-					point.latitude = [[stringArray objectAtIndex:0] doubleValue];
-					point.longitude = [[stringArray objectAtIndex:1] doubleValue];
-					[markerManager addMarker:marker AtLatLong:point];
-					[mapView moveToLatLong:point];
-					if(init)
-					{
-						if(point.latitude > ne.latitude)
-							ne.latitude = point.latitude;
-						if(point.latitude < sw.latitude)
-							sw.latitude = point.latitude;
-						if(point.longitude > ne.longitude)
-							ne.longitude = point.longitude;
-						if(point.longitude < sw.longitude)
-							sw.longitude = point.longitude;
-					}
-					else
-					{
-						init = true;
-						ne = point;
-						sw = point;
-					}
+					if(point.latitude > ne.latitude)
+						ne.latitude = point.latitude;
+					if(point.latitude < sw.latitude)
+						sw.latitude = point.latitude;
+					if(point.longitude > ne.longitude)
+						ne.longitude = point.longitude;
+					if(point.longitude < sw.longitude)
+						sw.longitude = point.longitude;
+				}
+				else
+				{
+					init = true;
+					ne = point;
+					sw = point;
 				}
 			}
 		}
@@ -158,6 +149,35 @@
 
 	// load the map after it slides in
 	[self performSelector:@selector(loadMapView) withObject:nil afterDelay:0.3];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	_shouldReloadMarkers = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	if(_shouldReloadMarkers)
+	{
+		_shouldReloadMarkers = NO;
+		RMMarkerManager *markerManager = mapView.markerManager;
+		NSArray *markers = [markerManager getMarkers];
+
+		for(RMMarker *marker in markers)
+		{
+			NSMutableDictionary *theCall = (NSMutableDictionary *)marker.data;
+			NSString *latLong = [theCall objectForKey:CallLattitudeLongitude];
+			if(latLong && ![latLong isEqualToString:@"nil"])
+			{
+				CLLocationCoordinate2D point;
+				NSArray *stringArray = [latLong componentsSeparatedByString:@", "];
+				point.latitude = [[stringArray objectAtIndex:0] doubleValue];
+				point.longitude = [[stringArray objectAtIndex:1] doubleValue];
+				[markerManager moveMarker:marker AtLatLon:point];
+			}
+		}
+	}
 }
 
 - (void)geocacheDone:(Geocache *)geocache forCall:(NSMutableDictionary *)theCall
@@ -233,191 +253,39 @@
 	}
 }
 
-
-#define EMPTY_NSSTRING_IF_NULL(str) ((str) ? (str) : @"")
-- (NSString *)getAddressFromCall:(NSMutableDictionary *)theCall useHtml:(BOOL)useHtml
-{
-	NSString *streetNumber = [theCall objectForKey:CallStreetNumber];
-	NSString *apartmentNumber = [theCall objectForKey:CallApartmentNumber];
-	NSString *street = [theCall objectForKey:CallStreet];
-	NSString *city = [theCall objectForKey:CallCity];
-	NSString *state = [theCall objectForKey:CallState];
-	NSString *seperator = useHtml ? @"<br>" : @"";
-	
-	if(street && [street length] && 
-	   city && [city length] && 
-	   state && [state length])
-	{
-		apartmentNumber = EMPTY_NSSTRING_IF_NULL(apartmentNumber);
-		if(apartmentNumber.length)
-			apartmentNumber = [NSString stringWithFormat:@"(%@)", apartmentNumber];
-		streetNumber = EMPTY_NSSTRING_IF_NULL(streetNumber);
-		street = EMPTY_NSSTRING_IF_NULL(street);
-		city = EMPTY_NSSTRING_IF_NULL(city);
-		state = EMPTY_NSSTRING_IF_NULL(state);
-		return([NSString stringWithFormat:@"%@ %@ %@ %@%@, %@", streetNumber, apartmentNumber, street, seperator, city, state]);
-	}
-	return(nil);
-}
-
-- (NSString *)getInfoFromCall:(NSMutableDictionary *)theCall 
-{
-	NSString *name = [theCall objectForKey:CallName];
-	NSString *address = [self getAddressFromCall:theCall useHtml:YES];
-	if(name || address)
-	{
-		name = EMPTY_NSSTRING_IF_NULL(name);
-		address = EMPTY_NSSTRING_IF_NULL(address);
-		return([NSString stringWithFormat:@"%@ %@", name, address]);
-	}
-	return(@"");
-}
-
-
-
-#ifdef USEMAPVIEW
-#pragma mark UIWebView delegate methods
-
-- (void)timer
-{
-	NSInteger iter = [[mapView.map evalJS:@"getNextResult()"] intValue];
-	if(iter == -2)
-	{
-		// there are unresolved addresses, keep going
-	}
-	else if(iter == -1)
-	{
-		// all done, dont need to keep calling the timer
-		return;
-	}
-	else
-	{
-		NSString *latLong = [mapView.map evalJS:[NSString stringWithFormat:@"getResult(%d)", iter]];
-		NSString *address = [mapView.map evalJS:[NSString stringWithFormat:@"getAddress(%d)", iter]];
-		NSMutableDictionary *foundCall = call;
-		if(foundCall == nil)
-		{
-			NSEnumerator *e = [[[[Settings sharedInstance] settings] objectForKey:SettingsCalls] objectEnumerator];
-			
-			while ( (foundCall = [e nextObject]) ) 
-			{
-				NSString *str = [self getAddressFromCall:foundCall useHtml:NO];
-				if(str && [str isEqualToString:address])
-				{
-					break;
-				}
-			}
-		}
-		if(foundCall)
-		{
-			[foundCall setObject:latLong forKey:CallLattitudeLongitude];
-			[[Settings sharedInstance] saveData];
-		}
-	}
-
-	[self performSelector:@selector(timer) withObject:self afterDelay:1.0];
-}
-
-- (void)mapViewDidFinishLoad:(MapWebView *)theWebView
-{
-	BOOL found = NO;
-	
-	[self stopProgressIndicator];
-
-	if(call)
-	{
-		NSString *str = [self getAddressFromCall:call useHtml:NO];
-		NSString *info = [self getInfoFromCall:call];
-		if(str)
-		{
-			NSString *latLong = [call objectForKey:CallLattitudeLongitude];
-			if(latLong)
-			{
-				NSString *script = [NSString stringWithFormat:@"showAddress(\"%@\", %@)", info, latLong];
-				VERBOSE(NSLog(@"%@", script);)
-				if([theWebView evalJS:script] == nil)
-				{
-					NSLog(@"script failure");
-				}
-			}
-			else
-			{
-				NSString *script = [NSString stringWithFormat:@"findAddress(\"%@\", \"%@\");", info, str];
-				VERBOSE(NSLog(@"%@", script);)
-				
-				if([theWebView evalJS:script] == nil)
-				{
-					NSLog(@"script failure");
-				}
-				found = YES;
-			}
-		}
-	}
-	else
-	{
-		NSEnumerator *e = [[[[Settings sharedInstance] settings] objectForKey:SettingsCalls] objectEnumerator];
-		NSMutableDictionary *theCall;
-		
-		while ( (theCall = [e nextObject]) ) 
-		{
-			NSString *str = [self getAddressFromCall:theCall useHtml:NO];
-			NSString *info = [self getInfoFromCall:theCall];
-			if(str)
-			{
-				NSString *latLong = [theCall objectForKey:CallLattitudeLongitude];
-				if(latLong)
-				{
-					NSString *script = [NSString stringWithFormat:@"showAddress(\"%@\", %@);", info, latLong];
-					VERBOSE(NSLog(@"%@", script);)
-					if([theWebView evalJS:script] == nil)
-					{
-						NSLog(@"script failure");
-					}
-				}
-				else
-				{
-					NSString *script = [NSString stringWithFormat:@"findAddress(\"%@\", \"%@\");", info, str];
-					VERBOSE(NSLog(@"%@", script);)
-					
-					if([theWebView evalJS:script] == nil)
-					{
-						NSLog(@"script failure");
-					}
-					found = YES;
-				}
-			}
-		}
-	}
-	if(found)
-	{
-		[self performSelector:@selector(timer) withObject:self afterDelay:1.0];
-	}
-}
-
-- (void) mapView:(MapWebView *)theWebView didFailLoadWithError:(NSError *)error;
-{
-	[self stopProgressIndicator];
-
-	// report the error inside the webview
-	NSString* errorString = [NSString stringWithFormat:
-							 @"<html><center><font size=+5 color='red'>An error occurred:<br>%@</font></center></html>",
-							 error.localizedDescription];
-	[theWebView loadHTMLString:errorString baseURL:nil];
-}
-
-- (void) mapZoomUpdatedTo:(int)zoomLevel
+- (void) beforeMapMove: (RMMapView*) map
 {
 }
 
-- (void) mapCenterUpdatedToLatLng:(GLatLng)latlng
+- (void) afterMapMove: (RMMapView*) map
 {
 }
 
-- (void) mapCenterUpdatedToPixel:(GPoint)pixel
+- (void) beforeMapZoom: (RMMapView*) map byFactor: (float) zoomFactor near:(CGPoint) center
 {
 }
 
-#endif
+- (void) afterMapZoom: (RMMapView*) map byFactor: (float) zoomFactor near:(CGPoint) center
+{
+}
+
+- (void) doubleTapOnMap: (RMMapView*) map At: (CGPoint) point
+{
+}
+
+- (void) tapOnLabelForMarker: (RMMarker*) marker onMap: (RMMapView*) map
+{
+}
+
+- (void) dragMarkerPosition: (RMMarker*) marker onMap: (RMMapView*)map position:(CGPoint)position
+{
+}
+
+- (void) afterMapTouch: (RMMapView*) map
+{
+}
+
+
 - (BOOL)respondsToSelector:(SEL)selector
 {
 	BOOL ret = [super respondsToSelector:selector];
@@ -487,42 +355,3 @@
 
 
 @end
-
-// ABQIAAAA8ZaIKMZcvWHNxFL4rpcPlxR4MgLgohEpMJOdpRIqyc07Qp4zFRRNU4wM18YEuS7Vh24_NSPNY8OJVA
-
-
-
-#if 0
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <title>Google Maps JavaScript API Example - simple</title>
-  <script src="http://maps.google.com/maps?file=api&v=1&key=ABQIAAAAy6MBiqsAuMYcRcIXE-SdkhQI513BVj7Itr7WPMGFabbrMAySrRTqrYgzCD2NDBq2Npe_EDAW--Is2A" type="text/javascript"></script>
-
-<!-- Make the document body take up the full screen --> <style type="text/css"> v\:* {behavior:url(#default#VML);} html, body {width: 100%; height: 100%} body {margin-top: 0px; margin-right: 0px; margin-left: 0px; margin-bottom: 0px} </style>
-
-</head>
-<body>
-
-  <!-- Declare the div, make it take up the full document body -->
-  <div id="map" style="width: 100%; height: 100%;"></div>
-
-  <script type="text/javascript">
-    //<![CDATA[        
-
-    var map;
-    if (GBrowserIsCompatible()) {
-      map = new GMap(document.getElementById("map"));
-      map.centerAndZoom(new GPoint(-122.141944, 36.441944), 4);
-
-// Monitor the window resize event and let the map know when it occurs if (window.attachEvent) { window.attachEvent("onresize", function() {this.map.onResize()} ); } else { window.addEventListener("resize", function() {this.map.onResize()} , false); }
-
-    }
-
-    //]]>
-  </script>
-
-</body></html>
-
-#endif
-
