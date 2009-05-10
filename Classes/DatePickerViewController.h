f352367fca1e6e47b25c6bf7eb56f07a89b785f6
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-05-10 04:40:08 +0000

    updated copyright notice

--- Classes/DatePickerViewController.h
+++ Classes/DatePickerViewController.h
@@ -5,6 +5,13 @@
 //  Created by Brent Priddy on 8/15/08.
 //  Copyright 2008 Priddy Software, LLC. All rights reserved.
 //
+//  Permission is NOT given to use this source code file in any
+//  project, commercial or otherwise, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in non compiled references
+//  MUST attribute the source (Priddy Software, LLC).  This file (in part or whole) 
+//  is NOT allowed to be used in a compiled or scripted program.
+//
 
 #import <UIKit/UIKit.h>
 #import "DatePickerViewControllerDelegate.h"
