73fa3ff390cb908fefb040633b25bf8c7f5af277
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-10-19 06:06:54 +0000

    2.4.9 to beta testers

---
+++ tags/2.4.9/Classes/SettingsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsTableViewController.h
+//  MyTime
+//
+//  Created by Brent Priddy on 9/18/09.
+//  Copyright 2009 Priddy Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GenericTableViewController.h"
+
+@interface SettingsTableViewController : GenericTableViewController <UIActionSheetDelegate>
+{
+}
+
+@end
