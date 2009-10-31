99e4d6d13cf0a142f3dc7003cf205df01aacb55f
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-10-31 04:05:54 +0000

    2.5.0 to AppStore

---
+++ tags/2.5.0/Classes/SubmitTranslationFile.m
@@ -0,0 +1,122 @@
+//
+//  SubmitTranslationFile.m
+//  MyTime
+//
+//  Created by Brent Priddy on 7/28/09.
+//  Copyright 2009 Priddy Software, LLC. All rights reserved.
+//
+
+#import "SubmitTranslationFile.h"
+#import "HTTPServer.h"
+
+
+@implementation SubmitTranslationFile
+
+//
+// load
+//
+// Implementing the load method and invoking
+// [HTTPResponseHandler registerHandler:self] causes HTTPResponseHandler
+// to register this class in the list of registered HTTP response handlers.
+//
++ (void)load
+{
+	[HTTPResponseHandler registerHandler:self];
+}
+
+//
+// canHandleRequest:method:url:headerFields:
+//
+// Class method to determine if the response handler class can handle
+// a given request.
+//
+// Parameters:
+//    aRequest - the request
+//    requestMethod - the request method
+//    requestURL - the request URL
+//    requestHeaderFields - the request headers
+//
+// returns YES (if the handler can handle the request), NO (otherwise)
+//
++ (BOOL)canHandleRequest:(CFHTTPMessageRef)aRequest
+				  method:(NSString *)requestMethod
+					 url:(NSURL *)requestURL
+			headerFields:(NSDictionary *)requestHeaderFields
+{
+	if ([requestURL.path isEqualToString:@"/translation.html"])
+	{
+		return YES;
+	}
+	if ([requestURL.path isEqualToString:@"/"])
+	{
+		return YES;
+	}
+	if ([requestURL.path isEqualToString:@""])
+	{
+		return YES;
+	}
+	
+	return NO;
+}
+
+- (void)sendPage
+{
+	NSString *errorString = nil;
+	
+	for(NSDictionary *entry in self.variableArray)
+	{
+		if([@"file" isEqualToString:[entry objectForKey:MultipartVariableName]])
+		{
+			NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
+			
+			
+			NSString *directory = [NSString stringWithFormat:@"%@.lproj", [[NSLocale currentLocale] localeIdentifier]];
+			NSString *filename = @"Localizable.strings";
+			NSFileManager *fileManager = [NSFileManager defaultManager];
+			NSMutableString *bundlePath = [NSMutableString stringWithString:[[[paths objectAtIndex:0] stringByAppendingPathComponent:@"translation.bundle/Contents/Resources/"] stringByAppendingPathComponent:directory]];
+			
+			if(![fileManager createDirectoryAtPath:bundlePath withIntermediateDirectories:YES attributes:nil error:nil])
+			{
+				errorString = [NSString stringWithFormat:@"could not create directory at %@", bundlePath];
+				NSLog(@"%@", errorString);
+			}
+			else
+			{
+				[bundlePath appendFormat:@"/%@", filename];
+				if(![fileManager moveItemAtPath:[entry objectForKey:MultipartVariableTempFilename] toPath:bundlePath error:nil])
+				{
+					NSLog(@"did not write file");
+				}
+			}
+		}
+	}
+	
+	NSData *fileData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"]];
+	CFHTTPMessageRef response = CFHTTPMessageCreateResponse(kCFAllocatorDefault, 200, NULL, kCFHTTPVersion1_0);
+	CFHTTPMessageSetHeaderFieldValue(response, (CFStringRef)@"Content-Type", (CFStringRef)@"text/html");
+	CFHTTPMessageSetHeaderFieldValue(response, (CFStringRef)@"Connection", (CFStringRef)@"close");
+	CFHTTPMessageSetHeaderFieldValue(response,
+									 (CFStringRef)@"Content-Length",
+									 (CFStringRef)[NSString stringWithFormat:@"%ld", fileData.length + errorString.length]);
+	CFDataRef headerData = CFHTTPMessageCopySerializedMessage(response);
+	
+	@try
+	{
+		[fileHandle writeData:(NSData *)headerData];
+		[fileHandle writeData:[errorString dataUsingEncoding:NSUTF8StringEncoding]];
+		[fileHandle writeData:fileData];
+	}
+	@catch (NSException *exception)
+	{
+		// Ignore the exception, it normally just means the client
+		// closed the connection from the other end.
+	}
+	@finally
+	{
+		CFRelease(response);
+		CFRelease(headerData);
+		[server closeHandler:self];
+	}
+}
+
+@end
