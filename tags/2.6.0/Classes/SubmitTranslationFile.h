7c2dcfb0905f81069b3824bfcded37a0f9fb0960
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2009-12-19 05:30:50 +0000

    2.6.0 to AppStore

---
+++ tags/2.6.0/Classes/SubmitTranslationFile.h
@@ -0,0 +1,16 @@
+//
+//  SubmitTranslationFile.h
+//  MyTime
+//
+//  Created by Brent Priddy on 7/28/09.
+//  Copyright 2009 Priddy Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HttpMultipartResponseHandler.h"
+
+@interface SubmitTranslationFile : HttpMultipartResponseHandler 
+{
+}
+
+@end
