6bb49d26bf340d714bcae1d7940f7fddcb65aa20
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2010-07-22 05:11:27 +0000

    2.8.4 to beta testers

---
+++ trunk/MyTime.xcdatamodeld/MTTimeEntry.m
@@ -0,0 +1,19 @@
+// 
+//  MTTimeEntry.m
+//  MyTime
+//
+//  Created by Brent Priddy on 5/28/10.
+//  Copyright 2010 Priddy Software, LLC. All rights reserved.
+//
+
+#import "MTTimeEntry.h"
+
+#import "MTTimeType.h"
+
+@implementation MTTimeEntry 
+
+@dynamic minutes;
+@dynamic date;
+@dynamic type;
+
+@end
