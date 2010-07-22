74c4fee74a8247742b249893f8225db0eebce96a
toopriddy <toopriddy@2aab8e37-3a46-0410-83de-ebf96cde5751>
2010-07-22 03:51:45 +0000

    2.8.4 to beta testers

---
+++ tags/test-2.8.4/MyTime.xcdatamodeld/MTStatisticsAdjustment.m
@@ -0,0 +1,20 @@
+// 
+//  MTStatisticsAdjustment.m
+//  MyTime
+//
+//  Created by Brent Priddy on 5/28/10.
+//  Copyright 2010 Priddy Software, LLC. All rights reserved.
+//
+
+#import "MTStatisticsAdjustment.h"
+
+#import "MTUser.h"
+
+@implementation MTStatisticsAdjustment 
+
+@dynamic timestamp;
+@dynamic type;
+@dynamic adjustment;
+@dynamic user;
+
+@end
